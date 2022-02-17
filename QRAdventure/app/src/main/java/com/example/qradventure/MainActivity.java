3550fe4406906a05c031d2541ce5eb0a52243cf2
tradke <tylerradke15@gmail.com>
2022-02-17 11:56:50 -0700

    Pushing MainActivity changes

--- QRAdventure/app/src/main/java/com/example/qradventure/MainActivity.java
+++ QRAdventure/app/src/main/java/com/example/qradventure/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.qradventure;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
+
+        // Get a top level reference to the collection
+        final CollectionReference collectionReference = db.collection("Account");
+
+        // hashmap
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("username", "mlee1");
+
+        // test addition
+        collectionReference
+                .document("username")
+                .set(data);
+
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable
+                    FirebaseFirestoreException error) {
+            }
+        });
+
     }
 }
