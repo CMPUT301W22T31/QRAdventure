3f6cb3fc4c9165b0751a172029f79ea6773557dc
tradke <tylerradke15@gmail.com>
2022-03-06 19:55:00 -0700

    Core Search Functionality
    Core search functionality works
    click listener and navigation to Profile Activity works
    TODO: Polish UI, extend listview

--- QRAdventure/app/src/main/java/com/example/qradventure/ProfileActivity.java
+++ QRAdventure/app/src/main/java/com/example/qradventure/ProfileActivity.java
@@ -5,22 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 /**
  * Activity displaying the profile of any player. Anyone can access this activity.
  */
 public class ProfileActivity extends AppCompatActivity {
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-
         // unpack intent to get account username
+        Intent intent = getIntent();
+        username = intent.getStringExtra(getString(R.string.EXTRA_USERNAME));
+
         // query DB for username. Pull relevant fields to display
-        setTitle("USERNAME123456789s profile");
+        // TESTING: use dummy data
+        setTextViews(username, "March6", "March6", "3");
 
     }
 
@@ -31,5 +36,26 @@ public class ProfileActivity extends AppCompatActivity {
     public void goToViewCodes(View view) {
         Intent intent = new Intent(this, ViewCodesActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Updates the textviews to display user data
+     * @param name - account username
+     * @param email - account email
+     * @param phone - account phone #
+     * @param score - account score
+     */
+    public void setTextViews(String name, String email, String phone, String score) {
+        // get reference to the textviews
+        TextView tvUsername = findViewById(R.id.tvUsername);
+        TextView tvPhone = findViewById(R.id.tvPhone);
+        TextView tvEmail = findViewById(R.id.tvEmail);
+        TextView tvTotalScore = findViewById(R.id.tvTotalScore);
+
+        // set textview text
+        tvUsername.setText(name);
+        tvPhone.setText(email);
+        tvEmail.setText(phone);
+        tvTotalScore.setText(score);
     }
 }
--- QRAdventure/app/src/main/java/com/example/qradventure/SearchPlayersActivity.java
+++ QRAdventure/app/src/main/java/com/example/qradventure/SearchPlayersActivity.java
@@ -1,36 +1,133 @@
 package com.example.qradventure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 /**
  * Activity that lets anyone search for other players by their username
  */
 public class SearchPlayersActivity extends AppCompatActivity {
-    String username;
+    ListView playerListView;
+    ArrayList<String> playerNames;
+    ArrayAdapter<String> usernameAdapter;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_players);
-        setTitle("Search Players by Username");
 
+        // Initialize data, listview, adapter
+        playerListView = findViewById(R.id.username_list);
+        playerNames = new ArrayList<>();
+        usernameAdapter = new ArrayAdapter<>(this, R.layout.username_list, playerNames);
+        playerListView.setAdapter(usernameAdapter);
+
+        // set up on click listener
+        playerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                String username = (String) playerListView.getItemAtPosition(pos);
+                goToProfile(username);
+            }
+        });
 
     }
 
     /**
-     * **PLACEHOLDER**
-     * Temporary solution to access the Profile activity.
-     * Button that sends you to Profile Activity
+     * Sends app to the Profile Activity
+     * Includes the username in the intent
+     * @param username - username of selected profile
      */
-    public void goToProfile(View view) {
+    public void goToProfile(String username) {
         Intent intent = new Intent(this, ProfileActivity.class);
-        // put username string into intent
+        intent.putExtra(getString(R.string.EXTRA_USERNAME), username);
         startActivity(intent);
     }
+
+    /**
+     * Called when search button is clicked
+     * Query for an account with a username similar to the editText input
+     * @param v - (unused)
+     */
+    public void queryUsername(View v) {
+        // clear current results
+        playerNames.clear();
+        usernameAdapter.notifyDataSetChanged();
+
+        // get username from editText
+        EditText tvUsername = findViewById(R.id.etUsername);
+        String username = tvUsername.getText().toString();
+
+        if (username.matches("")) {
+            return;
+        }
+
+        // query AccountDB for documents that are similar to username
+        db = FirebaseFirestore.getInstance();
+        db.collection("AccountDB")
+                // query where document name starts with username
+                .whereGreaterThanOrEqualTo("__name__", username)
+                .whereLessThanOrEqualTo("__name__", username+"\uf8ff")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().isEmpty()) {
+                                // no results returned, notify via toast
+                                Context context = getApplicationContext();
+                                CharSequence text = "No results found!";
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(context, text, duration);
+                                toast.show();
+                            } else {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    if (document.exists()) {
+                                        playerNames.add(document.getId());
+                                    } else {
+                                        // crash protection & log. TODO: Revise
+                                        Log.d("SearchPlayersActivity", "document dne");
+                                    }
+                                }
+                            }
+                        } else {
+                            // Query failed. Temporarily: display a toast
+                            Context context = getApplicationContext();
+                            CharSequence text = "Error: Query Failed!";
+                            int duration = Toast.LENGTH_SHORT;
+                            Toast toast = Toast.makeText(context, text, duration);
+                            toast.show();
+                        }
+                        // update listview adapter
+                        usernameAdapter.notifyDataSetChanged();
+                    }
+                });
+    }
+
+
 
 }
