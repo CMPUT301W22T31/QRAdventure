adb885b969542c5d0567da06d426c82aa1358316
tradke <tylerradke15@gmail.com>
2022-03-10 15:58:42 -0700

    Drop Keyboard on Search

--- QRAdventure/app/src/main/java/com/example/qradventure/SearchPlayersActivity.java
+++ QRAdventure/app/src/main/java/com/example/qradventure/SearchPlayersActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -107,6 +109,11 @@ public class SearchPlayersActivity extends AppCompatActivity {
      */
     public void queryUsername(View v) {
         // drop keyboard
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
 
         // clear current results
         playerNames.clear();
