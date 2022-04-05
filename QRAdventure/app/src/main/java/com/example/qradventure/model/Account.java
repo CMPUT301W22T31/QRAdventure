b057fde4030509ffe281f4ae73b8e24565ebf335
Logan Jones <83597971+LogarithmicJones@users.noreply.github.com>
2022-04-05 15:00:37 -0600

    Update Account.java

--- QRAdventure/app/src/main/java/com/example/qradventure/model/Account.java
+++ QRAdventure/app/src/main/java/com/example/qradventure/model/Account.java
@@ -33,6 +33,12 @@ public class Account {
      *      Collection of Records that this user has scanned
      */
     public Account(String username, String email, String phoneNumber, String loginQR, String statusQR, ArrayList<Record> myRecords) {
+        String phone = phoneNumber;
+
+        //if user entered phone number
+        if (!phone.equals("")) {
+            phone = phoneNumber.substring(0,3) + " " + phoneNumber.substring(3,6) + " " + phoneNumber.substring(6);
+        }
         this.username = username;
         this.email = email;
         this.phoneNumber = phoneNumber;
@@ -50,6 +56,12 @@ public class Account {
      * @param statusQR
      */
     public Account(String username, String email, String phoneNumber, String loginQR, String statusQR) {
+        String phone = phoneNumber;
+
+        //if user entered phone number
+        if (!phone.equals("")) {
+            phone = phoneNumber.substring(0, 3) + " " + phoneNumber.substring(3, 6) + " " + phoneNumber.substring(6);
+        }
         this.username = username;
         this.email = email;
         this.phoneNumber = phoneNumber;
